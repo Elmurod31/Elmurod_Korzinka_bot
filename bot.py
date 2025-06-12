7ff07a823e0a85eb8e4e97dd82de0f256da05ee7
acer <elmurodpro@gmail.com>
2025-06-12 10:05:38 +0500

    push

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 
 from commands import router as commands_router
 from handlears import router as handlears_router
-# from aiogram.client.session.aiohttp import AiohttpSession
-# session = AiohttpSession(proxy="http://proxy.server:3128")
+from aiogram.client.session.aiohttp import AiohttpSession
+session = AiohttpSession(proxy="http://proxy.server:3128")
 load_dotenv()
 
 TOKEN = getenv("TOKEN")
-# bot = Bot(token=TOKEN, session=session)
+bot = Bot(token=TOKEN, session=session)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
